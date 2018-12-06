744f192009bc40b1213227059ae28adc80f3125e
Dallon Feldner <dfeldner@phunware.com>
2018-12-06 12:58:39 -0600

    Day 1 - Part Two

--- src/challenges/day-01-chronal-calibration.ts
+++ src/challenges/day-01-chronal-calibration.ts
@@ -1,13 +1,32 @@
 const REGEX = /^([+-])([0-9]+)/;
 
+const applyAdjustment = (adjustment: string, frequency: number): number => {
+  const matches = adjustment.match(REGEX);
+  if (!matches) throw new Error(`Bad adjustment format: ${adjustment}`);
+  const sign = matches[1] === '-' ? -1 : 1;
+  const adjustmentAmount = parseInt(matches[2], 10);
+  return frequency + adjustmentAmount * sign;
+};
+
 export const getResultingFrequency = (input: string[]): number => {
   let frequency = 0;
   input.forEach(adjustment => {
-    const matches = adjustment.match(REGEX);
-    if (!matches) throw new Error(`Bad adjustment format: ${adjustment}`);
-    const sign = matches[1] === '-' ? -1 : 1;
-    const adjustmentAmount = parseInt(matches[2], 10);
-    frequency += adjustmentAmount * sign;
+    frequency = applyAdjustment(adjustment, frequency);
   });
   return frequency;
+};
+
+export const getFirstRepeatedFrequency = (input: string[]): number => {
+  let currentFrequency = 0;
+  let index = 0;
+  const seenFrequencies = new Set<number>([0]);
+  while (true) {
+    const adjustment = input[index];
+    currentFrequency = applyAdjustment(adjustment, currentFrequency);
+    if (seenFrequencies.has(currentFrequency)) {
+      return currentFrequency;
+    }
+    seenFrequencies.add(currentFrequency);
+    index = (index + 1) % input.length;
+  }
 };
