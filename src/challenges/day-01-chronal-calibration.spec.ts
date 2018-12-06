1bad1f2dab32e8d1db35feddf4d03c2cd66f7904
Dallon Feldner <dfeldner@phunware.com>
2018-12-06 12:06:25 -0600

    Day 1 Part 1 Answer

--- src/challenges/day-01-chronal-calibration.spec.ts
+++ src/challenges/day-01-chronal-calibration.spec.ts
@@ -1,21 +1,27 @@
 import { getResultingFrequency } from './day-01-chronal-calibration';
-import { parseLines } from '../utils/readLines';
+import { parseLines, readLines } from '../utils/readLines';
 
-it('canon example', () => {
-  const data = parseLines(
-    `
+describe('part 1', () => {
+  it('canon example', () => {
+    const data = parseLines(
+      `
 +1
 -2
 +3
 +1
-`,
-    { filterNulls: true }
-  );
-  expect(getResultingFrequency(data)).toBe(3);
-});
+`
+    );
+    expect(getResultingFrequency(data)).toBe(3);
+  });
 
-it('more examples', () => {
-  expect(getResultingFrequency(['+1', '+1', '+1'])).toBe(3);
-  expect(getResultingFrequency(['+1', '+1', '-2'])).toBe(0);
-  expect(getResultingFrequency(['-1', '-2', '-3'])).toBe(-6);
+  it('more examples', () => {
+    expect(getResultingFrequency(['+1', '+1', '+1'])).toBe(3);
+    expect(getResultingFrequency(['+1', '+1', '-2'])).toBe(0);
+    expect(getResultingFrequency(['-1', '-2', '-3'])).toBe(-6);
+  });
+
+  it('answer', () => {
+    const input = readLines('./day-01-data.txt', __dirname);
+    expect(getResultingFrequency(input)).toMatchInlineSnapshot(`536`);
+  });
 });
