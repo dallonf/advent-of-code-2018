dcc76b524418e94b1d60136b3ca721d3a6077295
Dallon Feldner <dfeldner@phunware.com>
2018-12-12 11:12:53 -0600

    Can parse Begins Shift records

--- src/challenges/day-04-repose-record.ts
+++ src/challenges/day-04-repose-record.ts
@@ -15,25 +15,36 @@ type GuardRecord =
       time: moment.Moment;
     };
 
-const REGEX_GENERIC = /\[([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})\] (.*)/;
+const REGEX_GENERIC = /^\[([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})\] (.*)$/;
+const REGEX_BEGINS_SHIFT = /^Guard #([0-9]+) begins shift$/;
 export const parseRecord = (input: string): GuardRecord => {
   const match = input.match(REGEX_GENERIC);
   if (!match) {
     throw new Error(`Can't parse record: ${input}`);
   }
   const time = moment(match[1]);
+  const body = match[2];
 
-  if (match[2] === 'falls asleep') {
+  if (body === 'falls asleep') {
     return {
       type: 'fallsAsleep',
       time,
     };
-  } else if (match[2] === 'wakes up') {
+  } else if (body === 'wakes up') {
     return {
       type: 'wakesUp',
       time,
     };
   } else {
-    throw new Error(`Unrecognized record body: ${match[2]}`);
+    const beginsShiftMatch = body.match(REGEX_BEGINS_SHIFT);
+    if (beginsShiftMatch) {
+      return {
+        type: 'beginsShift',
+        guardId: parseInt(beginsShiftMatch[1], 10),
+        time,
+      };
+    } else {
+      throw new Error(`Unrecognized record body: ${body}`);
+    }
   }
 };
