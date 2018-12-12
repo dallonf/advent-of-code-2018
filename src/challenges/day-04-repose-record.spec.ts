747b0857d6d77bbedc5610f287e28bc3c3637cd0
Dallon Feldner <dfeldner@phunware.com>
2018-12-12 11:02:41 -0600

    Can parse sleep / wake statuses

--- src/challenges/day-04-repose-record.spec.ts
+++ src/challenges/day-04-repose-record.spec.ts
@@ -2,9 +2,15 @@ import moment from 'moment';
 import { parseRecord } from './day-04-repose-record';
 
 describe('parseRecord', () => {
-  it('parses a fallsAlseep record', () => {
+  it('parses a fallsAsleep record', () => {
     expect(parseRecord('[1518-11-01 00:05] falls asleep')).toEqual({
       type: 'fallsAsleep',
+      time: moment('1518-11-01 00:05'),
+    });
+  });
+  it('parses a wakesUp record', () => {
+    expect(parseRecord('[1518-11-01 00:05] wakes up')).toEqual({
+      type: 'wakesUp',
       time: moment('1518-11-01 00:05'),
     });
   });
--- src/challenges/day-04-repose-record.ts
+++ src/challenges/day-04-repose-record.ts
@@ -15,5 +15,25 @@ type GuardRecord =
       time: moment.Moment;
     };
 
-const REGEX_GENERIC = /\[[0-9]{4}-{0-9}{2}\] (.*)/;
-export const parseRecord = (input: string): GuardRecord => {};
+const REGEX_GENERIC = /\[([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})\] (.*)/;
+export const parseRecord = (input: string): GuardRecord => {
+  const match = input.match(REGEX_GENERIC);
+  if (!match) {
+    throw new Error(`Can't parse record: ${input}`);
+  }
+  const time = moment(match[1]);
+
+  if (match[2] === 'falls asleep') {
+    return {
+      type: 'fallsAsleep',
+      time,
+    };
+  } else if (match[2] === 'wakes up') {
+    return {
+      type: 'wakesUp',
+      time,
+    };
+  } else {
+    throw new Error(`Unrecognized record body: ${match[2]}`);
+  }
+};
