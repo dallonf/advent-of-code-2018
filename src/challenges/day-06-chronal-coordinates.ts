397267187a0d78dc6cb344ab4303c5a394ca4405
dallonf <dallon.feldner@gmail.com>
2018-12-15 13:27:36 -0600

    Day 6 Part Two

--- src/challenges/day-06-chronal-coordinates.ts
+++ src/challenges/day-06-chronal-coordinates.ts
@@ -15,22 +15,31 @@ export const parseInput = (input: string) => {
   };
 };
 
-export const getLargestArea = (input: Point[]): number => {
+const getBounds = (input: Point[]) => {
   const minX = lodash.min(input.map(i => i.x))!;
   const minY = lodash.min(input.map(i => i.y))!;
   const maxX = lodash.max(input.map(i => i.x))!;
   const maxY = lodash.max(input.map(i => i.y))!;
+  return { minY, maxY, minX, maxX };
+};
+
+function manhattanDistance(point1: Point, point2: Point): number {
+  return Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y);
+}
+
+export const getLargestArea = (input: Point[]): number => {
+  const { minY, maxY, minX, maxX } = getBounds(input);
 
   const pointsWithIds = input.map((point, id) => ({ point, id }));
   const areas = new Map<number, number>();
   const infiniteAreasByPointId = new Set<number>();
 
   // loop through every cell in the interior grid
-  lodash.range(minY - 1, maxY + 2).map(y =>
-    lodash.range(minX - 1, maxX + 2).map(x => {
+  lodash.range(minY - 1, maxY + 2).forEach(y =>
+    lodash.range(minX - 1, maxX + 2).forEach(x => {
       const pointDistances = pointsWithIds.map(a => ({
         ...a,
-        distance: Math.abs(x - a.point.x) + Math.abs(y - a.point.y),
+        distance: manhattanDistance(a.point, { x, y }),
       }));
 
       const closestPoint = lodash.minBy(pointDistances, a => a.distance)!;
@@ -61,4 +70,19 @@ export const getLargestArea = (input: Point[]): number => {
   )!;
 };
 
-export const getClusterRegion = (input: Point[]) => {};
+export const getClusterRegion = (input: Point[], maxScore: number) => {
+  const { minY, maxY, minX, maxX } = getBounds(input);
+  let regionSize = 0;
+  lodash.range(minY, maxY + 1).map(y =>
+    lodash.range(minX, maxX + 1).map(x => {
+      const score = input
+        .map(a => manhattanDistance(a, { x, y }))
+        .reduce((a, b) => a + b);
+
+      if (score < maxScore) {
+        regionSize += 1;
+      }
+    })
+  );
+  return regionSize;
+};
