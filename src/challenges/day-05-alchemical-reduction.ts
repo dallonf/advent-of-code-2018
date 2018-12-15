e64e5872108836493a7fdf782bb073fccc56dc3a
dallonf <dallon.feldner@gmail.com>
2018-12-15 09:17:29 -0600

    Optimize Day 5

--- src/challenges/day-05-alchemical-reduction.ts
+++ src/challenges/day-05-alchemical-reduction.ts
@@ -12,7 +12,7 @@ const getCase = (input: string) => {
   }
 };
 
-export const reactionSingleStep = (input: string) => {
+const reactionSingleStep_internal = (input: string) => {
   let reactionIndex = -1;
   for (let i = 0; i < input.length - 1; i++) {
     const letter = input.charAt(i);
@@ -27,22 +27,36 @@ export const reactionSingleStep = (input: string) => {
   }
 
   if (reactionIndex === -1) {
-    return input;
+    return { result: input };
   } else {
-    const newString = [...input];
-    newString.splice(reactionIndex, 2);
-    return newString.join('');
+    const preSplit = input.slice(0, reactionIndex);
+    const postSplit = input.slice(reactionIndex + 2);
+    return { result: preSplit + postSplit, preSplit, postSplit };
+    // const newString = [...input];
+    // newString.splice(reactionIndex, 2);
+    // return { result: newString.join(''), reactionIndex };
   }
 };
 
+export const reactionSingleStep = (input: string) => {
+  return reactionSingleStep_internal(input).result;
+};
+
 export const reaction = (input: string): string => {
-  let prev = input;
-  let next = input;
+  let stable = '';
+  let unstable = input;
+
   while (true) {
-    next = reactionSingleStep(prev);
-    if (prev === next) {
-      return next;
+    const next = reactionSingleStep_internal(unstable);
+
+    if (!next.preSplit && !next.postSplit) {
+      return stable + next.result;
     }
-    prev = next;
+
+    // back up one after the point of the previous reaction;
+    // the units adjacent to the split might react
+    const fullPreSplit = stable + next.preSplit;
+    stable = fullPreSplit.slice(0, fullPreSplit.length - 1);
+    unstable = fullPreSplit.slice(fullPreSplit.length - 1) + next.postSplit;
   }
 };
