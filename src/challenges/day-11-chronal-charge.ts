7d2bc635bd4cbf23d721faa6bedb6acafd7106b2
Dallon Feldner <dfeldner@phunware.com>
2018-12-21 11:43:22 -0600

    Day 11 examples

--- src/challenges/day-11-chronal-charge.ts
+++ src/challenges/day-11-chronal-charge.ts
@@ -1,3 +1,5 @@
+import * as lodash from 'lodash';
+
 export const getPowerLevel = (
   x: number,
   y: number,
@@ -10,4 +12,30 @@ export const getPowerLevel = (
   const hundredsPlace =
     Math.floor(result / 100) - Math.floor(result / 1000) * 10;
   return hundredsPlace - 5;
+};
+
+const SELECTION_SIZE = 3;
+const GRID = lodash.flatMap(lodash.range(1, 301 - SELECTION_SIZE), x =>
+  lodash.range(1, 301 - SELECTION_SIZE).map(y => ({ x, y }))
+);
+export const getBestSquare = (
+  serialNumber: number
+): { x: number; y: number; totalPower: number } => {
+  const result = lodash.maxBy(
+    GRID.map(cell => {
+      let totalPower = 0;
+      for (let xDelta = 0; xDelta < SELECTION_SIZE; xDelta++) {
+        for (let yDelta = 0; yDelta < SELECTION_SIZE; yDelta++) {
+          totalPower += getPowerLevel(
+            cell.x + xDelta,
+            cell.y + yDelta,
+            serialNumber
+          );
+        }
+      }
+      return { ...cell, totalPower };
+    }),
+    a => a.totalPower
+  );
+  return result!;
 };
