c46020cf6df8344d892e9bbf19286d65bd3d7d57
Dallon Feldner <dfeldner@phunware.com>
2018-12-21 11:43:22 -0600

    Day 11 examples

--- src/challenges/day-11-chronal-charge.spec.ts
+++ src/challenges/day-11-chronal-charge.spec.ts
@@ -1,4 +1,4 @@
-import { getPowerLevel } from './day-11-chronal-charge';
+import { getPowerLevel, getBestSquare } from './day-11-chronal-charge';
 
 describe('Part One', () => {
   it('gets power level', () => {
@@ -8,5 +8,22 @@ describe('Part One', () => {
     expect(getPowerLevel(122, 79, 57)).toBe(-5);
     expect(getPowerLevel(217, 196, 39)).toBe(0);
     expect(getPowerLevel(101, 153, 71)).toBe(4);
+  });
+
+  it('gets best square (1)', () => {
+    const result = getBestSquare(18);
+    expect(result).toEqual({
+      x: 33,
+      y: 45,
+      totalPower: 29,
+    });
+  });
+  it('gets best square (2)', () => {
+    const result = getBestSquare(42);
+    expect(result).toEqual({
+      x: 21,
+      y: 61,
+      totalPower: 30,
+    });
   });
 });
