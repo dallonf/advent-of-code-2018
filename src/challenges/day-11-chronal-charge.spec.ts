1675dffb1eb990a45ee8cf53f13c7612e134587b
Dallon Feldner <dfeldner@phunware.com>
2018-12-21 18:02:56 -0600

    Use a SummedAreaTable to speed up Part Two

--- src/challenges/day-11-chronal-charge.spec.ts
+++ src/challenges/day-11-chronal-charge.spec.ts
@@ -2,6 +2,7 @@ import {
   getPowerLevel,
   getBestSquare,
   getBestSquareAndSize,
+  getSummedAreaTable,
 } from './day-11-chronal-charge';
 
 describe('Part One', () => {
@@ -12,6 +13,42 @@ describe('Part One', () => {
     expect(getPowerLevel(122, 79, 57)).toBe(-5);
     expect(getPowerLevel(217, 196, 39)).toBe(0);
     expect(getPowerLevel(101, 153, 71)).toBe(4);
+  });
+
+  describe('getSummedAreaTable', () => {
+    it('readFromTable, very small numbers', () => {
+      const table = getSummedAreaTable(18);
+      expect(table.readFromTable(1, 1)).toBe(getPowerLevel(1, 1, 18));
+      expect(table.readFromTable(2, 1)).toBe(
+        getPowerLevel(1, 1, 18) + getPowerLevel(2, 1, 18)
+      );
+      expect(table.readFromTable(1, 2)).toBe(
+        getPowerLevel(1, 1, 18) + getPowerLevel(1, 2, 18)
+      );
+      expect(table.readFromTable(2, 2)).toBe(
+        getPowerLevel(1, 1, 18) +
+          getPowerLevel(1, 2, 18) +
+          getPowerLevel(2, 1, 18) +
+          getPowerLevel(2, 2, 18)
+      );
+    });
+    it('getSum, very small numbers', () => {
+      const table = getSummedAreaTable(18);
+      expect(table.getSum(1, 1, 2, 2)).toBe(
+        getPowerLevel(1, 1, 18) +
+          getPowerLevel(1, 2, 18) +
+          getPowerLevel(2, 1, 18) +
+          getPowerLevel(2, 2, 18)
+      );
+    });
+    it('gets an exact square', () => {
+      const table = getSummedAreaTable(18);
+      expect(table.getSum(33, 45, 33, 45)).toBe(4);
+    });
+    it('get a sum', () => {
+      const table = getSummedAreaTable(18);
+      expect(table.getSum(33, 45, 35, 47)).toBe(29);
+    });
   });
 
   it('gets best square (1)', () => {
@@ -43,7 +80,7 @@ Object {
   });
 });
 
-describe.skip('Part Two', () => {
+describe('Part Two', () => {
   it('gets best square and size', () => {
     expect(getBestSquareAndSize(18)).toEqual({
       x: 90,
