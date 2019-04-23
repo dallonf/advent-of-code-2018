a8a998854448993e72fe9a44e3d62063d088ac9b
DallonF <dallon.feldner@gmail.com>
2019-04-23 17:51:21 -0500

    Day 12 Part Two

--- pysrc/challenges/day_12_subterranean_sustainability.py
+++ pysrc/challenges/day_12_subterranean_sustainability.py
@@ -61,15 +61,20 @@ def parse_generation(input, leftmost_pot=0):
     return result
 
 
-def stringify_pots(pots: Pots):
+def stringify_pots(pots: Pots, with_index=False):
     numbers = pots.keys()
     lowest_number, highest_number = min(numbers), max(numbers)
     output_list = list()
 
     for i in range(lowest_number, highest_number + 1):
-        output_list.append('#' if pots[i] else '.')
+        output_list.append('#' if pots.get(i, False) else '.')
 
-    return "".join(output_list)
+    output = "".join(output_list)
+
+    if with_index:
+        return str(lowest_number) + ":" + output
+    else:
+        return output
 
 
 def get_pattern_slice(pots: Pots, index: int):
@@ -83,6 +88,7 @@ def simulate_generation(pots: Pots, rules: Rules):
     numbers = pots.keys()
     # because the pattern-matching includes the two pots to the side of each pot, it's possible that
     # pots outside of the range represented could be affected
+    # assumption: there will never be a pattern of "..#..", which would affect an infinite number of pots
     lowest_number, highest_number = min(numbers) - 2, max(numbers) + 2
 
     next_generation = dict()
@@ -90,16 +96,39 @@ def simulate_generation(pots: Pots, rules: Rules):
     for i in range(lowest_number, highest_number + 1):
         pattern_slice = get_pattern_slice(pots, i)
         pattern_result = rules.get(pattern_slice, False)
-        next_generation[i] = pattern_result
+        if (pattern_result):
+            # don't actually save "False" to the list
+            next_generation[i] = pattern_result
 
     return next_generation
 
 
-def get_sum(input: List[str]):
+def get_sum(input: List[str], generations=20):
     parsed_input = parse_input(input)
     state = parsed_input.initial_state
 
-    for _ in range(20):
-        state = simulate_generation(state, parsed_input.rules)
+    generations_simulated = 0
 
-    return sum(i for i,v in state.items() if v)
+    def get_index(pots: Pots):
+        return min(pots.keys())
+
+    for _ in range(generations):
+        prev_state = state
+        state = simulate_generation(state, parsed_input.rules)
+        generations_simulated += 1
+        # If the pattern has not changed, bail out of the loop; it will never change again
+        # Assumption: every input will eventually reach a stable state
+        # It's theoretically possible that it could enter a loop between states
+        # But I don't have any inputs that could result in this behavior
+        # However, it IS quite likely that the pattern, although unchanging, may slowly migrate across the space
+        if stringify_pots(prev_state) == stringify_pots(state):
+            # figure out how much it's going to move each iteration
+            prev_index = get_index(prev_state)
+            new_index = get_index(state)
+            delta = new_index - prev_index
+            # figure out how much it's going to move in the remaining generations
+            full_distance = delta*(generations-generations_simulated)
+            state = { (i + full_distance): v for i, v in state.items() }
+            break
+
+    return sum(i for i, v in state.items() if v)
