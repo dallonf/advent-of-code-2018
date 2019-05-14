c35895f2cc1623b01f58932adc95f150761a894a
DallonF <dallon.feldner@gmail.com>
2019-05-14 15:06:24 -0500

    Basic collision detection

--- pysrc/challenges/day_13_mine_cart_madness.py
+++ pysrc/challenges/day_13_mine_cart_madness.py
@@ -1,13 +1,73 @@
+import typing
+
+
+class Cart(typing.NamedTuple):
+    id: int
+    position: typing.Tuple[int, int]
+    "^, V, <, >"
+    direction: str
+
+
 def parse_track(input_lines):
     track = {}
-    carts = []
+    carts = {}
 
     for y, line in enumerate(input_lines):
         for x, char in enumerate(line):
-            if char in ("/", "\\"):
+            if char in ("/", "\\", "+"):
                 track[(x, y)] = char
+            if char in ('^', 'V', '<', '>'):
+                new_cart = Cart(id=len(carts), position=(x, y), direction=char)
+                carts[new_cart.id] = new_cart
 
     return {
         'track': track,
         'carts': carts
+    }
+
+
+def simulate_tick(state):
+    carts_to_simulate = list(state['carts'].values())
+    carts_to_simulate.sort(key=lambda x: x.position)
+    next_tick_carts = state['carts'].copy()
+    collision = None
+
+    for cart in carts_to_simulate:
+        x, y = cart.position
+
+        def up():
+            return (x, y - 1)
+
+        def left():
+            return (x - 1, y)
+
+        def down():
+            return (x, y + 1)
+
+        def right():
+            return (x + 1, y)
+
+        directions = {
+            '^': up,
+            '<': left,
+            'V': down,
+            '>': right
+        }
+
+        new_position = directions[cart.direction]()
+
+        next_tick_carts[cart.id] = cart._replace(position=new_position)
+
+        colliding_cart = next((other_cart for other_cart in next_tick_carts.values()
+                               if other_cart.id != cart.id and other_cart.position == new_position), None)
+
+        if colliding_cart:
+            collision = colliding_cart.position
+
+    new_state = state.copy()
+    new_state['carts'] = next_tick_carts
+
+    return {
+        'collision': collision,
+        'state': new_state
     }
--- pysrc/challenges/day_13_mine_cart_madness_test.py
+++ pysrc/challenges/day_13_mine_cart_madness_test.py
@@ -1,17 +1,51 @@
 import pytest
 import pysrc.challenges.day_13_mine_cart_madness as day13
 
+SIMPLE_LOOP = [
+    "/----\\",
+    "|    |",
+    "|    |",
+    "\\----/"
+]
+
+SIMPLE_INTERSECTION = [
+    "/-----\\",
+    "|     |",
+    "| /--+--\\",
+    "|  |  |  |",
+    "\\--+--/  |",
+    "   |     |",
+    "   \\-----/",
+]
+
+LINEAR_CRASH = ["|", "V", "|", "|", "|", "^", "|"]
+
 
 class TestPartOne():
-    SIMPLE_LOOP = [
-        "/----\\",
-        "|    |",
-        "|    |",
-        "\\----/"
-    ]
 
     def test_parse_simple_loop(self):
-        result = day13.parse_track(self.SIMPLE_LOOP)
+        result = day13.parse_track(SIMPLE_LOOP)
         assert result['track'] == {
             (0, 0): '/', (5, 0): '\\', (0, 3): '\\', (5, 3): '/'
         }
+
+    def test_parse_simple_intersection(self):
+        result = day13.parse_track(SIMPLE_INTERSECTION)
+        assert result['track'] == {(0, 0): '/',
+                                   (6, 0): '\\',
+                                   (2, 2): '/',
+                                   (5, 2): '+',
+                                   (8, 2): '\\',
+                                   (0, 4): '\\',
+                                   (3, 4): '+',
+                                   (6, 4): '/',
+                                   (3, 6): '\\',
+                                   (9, 6): '/'}
+
+    def test_detect_collision(self):
+        state = day13.parse_track(LINEAR_CRASH)
+        for _ in range(2):
+            result = day13.simulate_tick(state)
+            state = result['state']
+
+        assert result['collision'] == (0, 3)
